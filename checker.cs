f72dff26e3cfd8c673f50c450292ccc5fffe01d9
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-07-07 23:55:52 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -13,48 +13,32 @@ class Checker
     
     static bool BatteryIsOk(float temperature, float state_of_charge, float chargeRate) 
     {
-        bool battery_status = ValidateTemperature(temperature) ? ValidateStateOfCharge(state_of_charge) ? ValidateChargeRate(chargeRate) ? true:false:false:false;
-        return battery_status;
-        /*if(!ValidateTemperate(temperature)) 
-        {
-            Console.WriteLine("Temperature is out of range!");
-            return false;
-        } 
-        else if(!ValidateStateOfCharge(state_of_charge)) 
-        {
-            Console.WriteLine("State of Charge is out of range!");
-            return false;
-        }
-        else if(!ValidateChargeRate(chargeRate)) 
-        {
-            Console.WriteLine("Charge Rate is out of range!");
-            return false;
-        }
-        return true;
-        */
-    }
+        //bool battery_status = ValidateTemperature(temperature) ? ValidateStateOfCharge(state_of_charge) ? ValidateChargeRate(chargeRate) ? true:false:false:false;
+        bool battery_status = ValidateTemperatureAndStateOfCharge(temperature,soc) ? (ValidateChargeRate(cr) ? true:false):false;            
+        return battery_status;       
+    }    
     
-    //Validate System Temperature
-    static bool ValidateTemperature(float temperature_data)
+    //Validate System Temperature and System of Charge
+     static bool ValidateTemperatureAndStateOfCharge(float temperature_data, float soc)
     {            
-        system_output = temperature_data < temp_min || temperature_data > temp_max ? false : true;      
-        if(!system_output)
-        {
-            Console.WriteLine("Temperature is out of range!");
-        }
-        return system_output;
-    }
-    
-    //Validate System State of Charge
-    static bool ValidateStateOfCharge(float soc_input)
-    {                 
-        system_output = soc_input < soc_min || soc_input > soc_max ? false : true;
-        if(!system_output)
-        {
-            Console.WriteLine("State of Charge is out of range!");
-        }
-       return system_output;
-    }
+            if (!ValidateData(temperature_data))
+            {
+                Console.WriteLine("Temperature is out of range!");
+                return false;
+            }
+            else
+            {
+                if (!ValidateData(soc))
+                {
+                    Console.WriteLine("State of Charge is out of range!");
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }      
+     }       
 
     //Validate Charge Rate
     static bool ValidateChargeRate(float charge_state_data)
@@ -66,6 +50,12 @@ class Checker
         }
         return system_output;
     }
+    
+    static bool ValidateData(float input)
+        {
+            system_output = input < temp_min || input > temp_max ? false : true;
+            return system_output;
+        }
         
     static void ExpectTrue(bool expression) 
     {
